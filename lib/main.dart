16813261efc4f0fbdb3dd4fe78fae91b9d6986e5
dafamalik <dafamalik08@gmail.com>
2021-08-05 11:38:59 +0700

    Change Title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Tokopedia',
+        title: 'Flutter Tokopedia Clone',
         home: MainTabbar()
     );
   }
